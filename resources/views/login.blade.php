a54b688471204fef71ae5f6c24c2db7b4abd1dd8
LaFii <sophannarith38@gmail.com>
2025-03-14 12:45:51 +0700

    style: update login view with responsive design and improved color scheme

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,26 +7,26 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         @vite('resources/css/app.css')
     </head>
-    <body class="bg-light">
-        <div class="container">
-            <div class="row my-5">
-                <div class="col-md-6 mx-auto">
+    <body class="flex items-center justify-center min-h-screen bg-[#001f3d] ">
+        <div class="container p-5 bg-[#001f3d]">
+            <div class="my-5 row">
+                <div class="mx-auto col-md-6">
                     @if(session('error'))
                         <div class="alert alert-danger">
                             {{ session('error') }}
                         </div>
                     @endif
-                    <div class="card shadow-sm p-5">
-                        <div class="card-header bg-white text-center">
-                            <h3 class="mt-2">
+                    <div class="p-5 bg-[#d99a00] shadow-sm card">
+                        <div class="text-center bg-[#001f3d] card-header">
+                            <h3 class="mt-2 bg-[#001f3d] text-white">
                                 Login
                             </h3>
                         </div>
                         <div class="card-body">
                             <form action="{{ route('admin.auth') }}" method="post">
                                 @csrf
-                                <div class="form-floating mb-3">
-                                    <input type="email" class="form-control" @error('email') is-invalid @enderror id="floatingInput"
+                                <div class="mb-3 form-floating">
+                                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="floatingInput"
                                         name="email" placeholder="name@example.com">
                                     <label for="floatingInput">Email address*</label>
                                 </div>
@@ -35,8 +35,8 @@
                                         {{ $message }}
                                     </div>
                                 @enderror
-                                <div class="form-floating mb-3">
-                                    <input type="password" class="form-control" @error('password') is-invalid @enderror
+                                <div class="mb-3 form-floating">
+                                    <input type="password" class="form-control @error('password') is-invalid @enderror"
                                         name="password" id="floatingPassword" placeholder="Password">
                                     <label for="floatingPassword">Password*</label>
                                 </div>
@@ -46,7 +46,7 @@
                                     </div>
                                 @enderror
                                 <div class="mb-2">
-                                    <button type="submit" class="btn btn-lg btn-dark">
+                                    <button type="submit" class="btn btn-lg bg-[#001f3d] text-white hover:bg-[#001a33] border-none">
                                         Sign in
                                     </button>
                                 </div>
